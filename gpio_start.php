2541d42d64f40e4efefaa7271b75b4a356464bc1
dudubass15 <carlos.contato00@gmail.com>
2019-01-20 13:43:36 -0200

    #20012019 - Add comando executar do php.

--- gpio_start.php
+++ gpio_start.php
@@ -1,7 +1,11 @@
 <?php
 
-    system('gpio-g mode 11 out');
-    system('gpio-g write 11 1');
+    $dado = system('gpio-g mode 11 out');
+    $dado.=system('gpio-g write 11 1');
+
+    exec($dado);
+
+    echo("Comando executado com sucesso !");
 
 
 ?>
--- gpio_stop.php
+++ gpio_stop.php
@@ -1,7 +1,11 @@
 <?php
 
-    system('gpio-g mode 11 out');
-    system('gpio-g write 11 0');
+    $dado = system('gpio-g mode 11 out');
+    $dado .= system('gpio-g write 11 0');
+
+    exec($dado);
+
+    echo("Comando executado com sucesso !");
 
 
 
