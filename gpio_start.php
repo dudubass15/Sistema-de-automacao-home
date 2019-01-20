eeadc082088923892f58e14f85cba907aa10177a
dudubass15 <carlos.contato00@gmail.com>
2019-01-20 15:47:56 -0200

    #20012019 - Correcao comando acionamento 05.

--- gpio_start.php
+++ gpio_start.php
@@ -1,9 +1,11 @@
 <?php
 
-    shell_exec('gpio-g mode 11 out');
-    shell_exec('gpio-g write 11 1');
+    // shell_exec('gpio-g mode 11 out');
+    // shell_exec('gpio-g write 11 1');
 
-    echo("Comando executado com sucesso !");
+    $a- exec("sudo python3 /home/pi/Documentos/rele_teste.py")
+
+    echo $a;
 
 
 ?>
