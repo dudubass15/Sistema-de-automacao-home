d35c30370a985c8be8d57c896429ab1cb4798fdf
dudubass15 <carlos.contato00@gmail.com>
2019-01-20 15:53:31 -0200

    #20012019 - Colocando ; no final do script.

--- gpio_start.php
+++ gpio_start.php
@@ -3,7 +3,7 @@
     // shell_exec('gpio-g mode 11 out');
     // shell_exec('gpio-g write 11 1');
 
-    $a- exec("sudo python3 /home/pi/Documentos/rele_teste.py")
+    $a = exec("sudo python3 /home/pi/Documentos/rele_teste.py");
 
     echo $a;
 
