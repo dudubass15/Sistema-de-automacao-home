fac3f5b7b933101e09415652f157c2e889902af9
dudubass15 <carlos.contato00@gmail.com>
2019-01-21 19:48:24 -0200

    #21012019 - Teste com script

--- gpio_stop.php
+++ gpio_stop.php
@@ -3,9 +3,9 @@
     //shell_exec('gpio-g mode 11 out');
     //shell_exec('gpio-g write 11 0');
 
-    $a- exec("sudo python3 /home/pi/Documentos/rele_teste.py");
+    system("sudo python3 /home/pi/Documentos/rele_teste.py");
 
-    echo $a;
+    echo("Comando enviado com sucesso !");
 
 
 
